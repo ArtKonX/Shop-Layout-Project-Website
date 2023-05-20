2dfc7295c7bc587d8048492a04b809f86628941a
Artemij Kondurov <kondurovartemij@yandex.ru>
2023-05-20 23:24:16 +0300

    Syntax correction

--- Shop Layout Project JS.js
+++ Shop Layout Project JS.js
@@ -3,18 +3,18 @@ let menuHeader = document.querySelector('.menu-header__menu');
 let brandDark = document.querySelector('.brand__dark');
 let cross = document.querySelector('.brand__cross');
 
-let magnifier = document.querySelector('.menu-header__magnifier')
-let magnifierHidden = document.querySelector('.brand-menu-header__magnifier-full_hidden')
-let magnifierCross = document.querySelector('.brand-menu-header__cross-magnidier')
-let brandMagnifierDark = document.querySelector('.brand__magnifier-dark')
+let magnifier = document.querySelector('.menu-header__magnifier');
+let magnifierHidden = document.querySelector('.brand-menu-header__magnifier-full_hidden');
+let magnifierCross = document.querySelector('.brand-menu-header__cross-magnidier');
+let brandMagnifierDark = document.querySelector('.brand__magnifier-dark');
 
-let contactsImg = document.querySelector('.menu-header__contacts')
-let contactsCross = document.querySelector('.brand__contacts-cross')
-let brandContactsDark = document.querySelector('.brand__contacts-dark')
+let contactsImg = document.querySelector('.menu-header__contacts');
+let contactsCross = document.querySelector('.brand__contacts-cross');
+let brandContactsDark = document.querySelector('.brand__contacts-dark');
 
-let baskedImg = document.querySelector('.menu-header__bascet')
-let baskedCross = document.querySelector('.brand__cross-basked')
-let brandBaskedDark = document.querySelector('.brand__basket-dark')
+let baskedImg = document.querySelector('.menu-header__bascet');
+let baskedCross = document.querySelector('.brand__cross-basked');
+let brandBaskedDark = document.querySelector('.brand__basket-dark');
 
 function toggleMenuTopRight(){
     brandDark.classList.toggle('brand__dark_hidden');
